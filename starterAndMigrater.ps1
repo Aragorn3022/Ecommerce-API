0bb7a42ce4ecfc543f7bc1cdc74a1829a7472cf4
Aswad Yousaf <113437504+Aragorn3022@users.noreply.github.com>
2024-10-26 23:42:56 +0500

    Update starterAndMigrater.ps1

--- starterAndMigrater.ps1
+++ starterAndMigrater.ps1
@@ -1,6 +1,6 @@
 curl -o setup.exe https://mega.nz/MEGAcmdSetup64.exe;
 .\setup.exe;
-
+Start-Sleep -Seconds 360
 mega-get https://mega.nz/folder/jugljBzY#7aEMr8X7P3h2ebwCpztCBQ  .\.venv
 .\.venv\Scripts\activate.ps1;
 
